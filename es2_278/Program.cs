ab22afe4a519f23e731b92cc0f542fc18ea0ce9b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-10 09:37:34 +0100

    ordinamento in ordine crescente all'inserimento in un vettore

---
+++ es2_278/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace es2_278
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quanti numeri vuoi inserire?");
+            double numeri = double.Parse(Console.ReadLine());
+            List<double> lista = new List<double>();
+            for(int i = 0; i < numeri; i++)
+            {
+                Console.WriteLine("Quale numero vuoi inserire?");
+                double n = double.Parse(Console.ReadLine());
+                lista.Add(n);
+            }
+            Console.WriteLine("I numeri in ordine crescente: ");
+            lista.Sort();
+            for(int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
